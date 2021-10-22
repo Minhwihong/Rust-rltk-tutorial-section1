e123c27866af068ad0ca8299f444bb0e291a5ef9
minhwihong <hidellok@gmail.com>
2021-10-22 11:51:56 +0900

    4th commit - 유저 캐릭터가 벽을 통과하지 않도록 수정

--- src/main.rs
+++ src/main.rs
@@ -76,10 +76,9 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
-
-
         self.run_systems();
         player_input(self, ctx);
+
 
         let map = self.ecs.fetch::<Vec<TileType>>();
         draw_map(&map, ctx);
@@ -150,10 +149,16 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World){
 
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
+
     }
 }
 
