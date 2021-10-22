a2f262bc05c24b5e88a5ffa27326bc1f5b7ac027
minhwihong <hidellok@gmail.com>
2021-10-22 12:04:59 +0900

    5th commit - seperate map.rs file

---
+++ src/map.rs
@@ -0,0 +1,78 @@
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+
+/*
+Clone : add a .clone() method to the type
+Copy : Allow a copy to be made programmatically ( tile1 = tile2 ) <- this is copy
+PartialEq : allows us to use == to see if two tile types match
+             if tile_type == TileType::Wall would fail to compile!
+ */
+#[derive(PartialEq, Copy, Clone)]
+pub enum TileType{
+    Wall, Floor
+}
+
+
+pub fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls.
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x,y);
+
+        if idx != xy_idx(40, 25){
+            map[idx] = TileType::Wall;
+        }
+    }
+    map
+}
+
+pub fn draw_map(map: &[TileType], ctx: &mut Rltk){
+    let mut y = 0;
+    let mut x = 0;
+
+    for tile in map.iter() {
+        //Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x,y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            },
+            TileType::Wall => {
+                ctx.set(x,y, RGB::from_f32(0.5, 1.0, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize*80) + x as usize
+}
+
+
+
